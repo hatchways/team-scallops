d154a599b221e301f02a4c0ab1b7f8a147c155a2
MarianaES <mariana.enriquez.s@gmail.com>
2021-10-15 20:49:36 -0500

    Add timestampss and totalPrice field

--- server/models/Request.js
+++ server/models/Request.js
@@ -2,19 +2,23 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const requestSchema = new mongoose.Schema({
-  customer: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  provider: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  start: { type: Date, required: true },
-  end: { type: Date, required: true },
-  accepted: { type: Boolean, default: false },
-  declined: { type: Boolean, default: false },
-  paid: { type: Boolean, default: false },
-  serviceType: {
-    type: String,
-    enum: ["Boarding", "House Sitting", "Day Care", "Walking"],
+const requestSchema = new mongoose.Schema(
+  {
+    customer: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    provider: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    start: { type: Date, required: true },
+    end: { type: Date, required: true },
+    accepted: { type: Boolean, default: false },
+    declined: { type: Boolean, default: false },
+    paid: { type: Boolean, default: false },
+    serviceType: {
+      type: String,
+      enum: ["Boarding", "House Sitting", "Day Care", "Walking"],
+    },
+    totalPrice: { type: Number, required: true },
+    rating: { type: Number },
   },
-  rating: { type: Number },
-});
+  { timestamps: true }
+);
 
 export default model("Request", requestSchema);
