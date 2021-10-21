df92376e23bb5ce759c5611f585ddc374c9f3dd0
Destin Martin <destintech@gmail.com>
2021-10-21 00:01:42 -0600

    Created cards for profile search page

--- client/src/pages/ProfileList/List.tsx
+++ client/src/pages/ProfileList/List.tsx
@@ -1,34 +1,102 @@
-import { Grid, Paper, Typography, Box, Card } from '@material-ui/core';
+import {
+  Grid,
+  Paper,
+  Typography,
+  Box,
+  Card,
+  TextField,
+  Button,
+  CardActions,
+  CardContent,
+  Divider,
+  CardMedia,
+  CardActionArea,
+  Avatar,
+} from '@material-ui/core';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
-//import useStyles from '../useStyles';
+import { Link } from 'react-router-dom';
+const profileImg = '775db5e79c5294846949f1f55059b53317f51e30.png';
+import useStyles from './useStyles';
 
 export default function List(): JSX.Element {
+  const classes = useStyles();
+
   const [state, setState] = useState({
     profiles: [],
   });
 
   useEffect(() => {
-    async function fetchProfile() {
-      await axios.get('/profile/all').then((response: any) => {
-        return setState(response.data.profiles);
+    function fetchProfile() {
+      axios.get('profile/all').then((response: any) => {
+        setState(response.data);
       });
     }
     fetchProfile();
   }, []);
   const profiles = state.profiles;
-  return (
-    <Box>
-      {profiles &&
-        profiles.map((profile, key) => {
-          return (
-            <Box key={key}>
-              <Typography>This</Typography>
-              <Typography>{profile}</Typography>
-            </Box>
-          );
-        })}
-    </Box>
-  );
+
+  if (!profiles) {
+    return <Box>loading</Box>;
+  } else {
+    console.log(state);
+    return (
+      <Box>
+        <Grid justify="center" container>
+          <Grid item>
+            <TextField
+              variant="outlined"
+              className={`${classes.search}`}
+              placeholder="Search by location..."
+            ></TextField>
+          </Grid>
+        </Grid>
+        <Grid container justify="center" spacing={6} className={`${classes.root}`}>
+          {profiles.map((profile, key) => {
+            const { firstName, lastName, userId, description } = profile;
+            return (
+              userId && (
+                <Grid item key={key}>
+                  <Card variant="outlined" key={key}>
+                    <CardActionArea>
+                      <Grid
+                        justify="center"
+                        alignItems="center"
+                        direction="column"
+                        container
+                        className={`${classes.card}`}
+                      >
+                        <Avatar src={profileImg} />
+
+                        <CardContent>
+                          <Typography>
+                            <Link to={`profile?user=${userId}`}> {userId}</Link>
+                          </Typography>
+                          <Typography>
+                            {firstName} {lastName}
+                          </Typography>
+                          <Typography> Rating</Typography>
+
+                          <Typography>{description}</Typography>
+                        </CardContent>
+                      </Grid>
+                    </CardActionArea>
+                    <Divider light />
+                    <Grid container spacing={2}>
+                      <Grid item>
+                        <Typography> Regina, Saskatchewan</Typography>
+                      </Grid>
+                      <Grid item>
+                        <Typography>$15/hr</Typography>
+                      </Grid>
+                    </Grid>
+                  </Card>
+                </Grid>
+              )
+            );
+          })}
+        </Grid>
+      </Box>
+    );
+  }
 }
--- client/src/pages/ProfileList/UseStyles.ts
+++ client/src/pages/ProfileList/UseStyles.ts
@@ -1,5 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles(() => ({
+  root: {
+    paddingLeft: '10vw',
+    paddingRight: '10vw',
+  },
+  card: {
+    paddingTop: '2em',
+  },
+  search: {
+    marginTop: '20vh',
+    marginBottom: '5vh',
+  },
+}));
 
 export default useStyles;
