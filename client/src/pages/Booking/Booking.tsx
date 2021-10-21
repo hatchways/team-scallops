8430e58f47ce4aa049c44b1e35ba7452afd7f0ff
MarianaES <mariana.enriquez.s@gmail.com>
2021-10-20 23:56:15 -0500

    Remove NavBar and change Card for Paper

--- client/src/pages/Booking/Booking.tsx
+++ client/src/pages/Booking/Booking.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 import { DatePicker, Day, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import { MuiThemeProvider } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
+import Paper from '@material-ui/core/Paper';
 
-import { useAuth } from '../../context/useAuthContext';
-import NavBar from '../../components/NavBar/NavBar';
 import BookingItem from '../../components/BookingItem/BookingItem';
 import useStyles from './useStyles';
 
@@ -47,18 +43,8 @@ const materialTheme = createMuiTheme({
 function Booking(): JSX.Element {
   const classes = useStyles();
 
-  const { loggedInUser } = useAuth();
-  const history = useHistory();
-
   const [bookedDays] = useState<number[]>([21, 25]);
   const [selectedDate, setDate] = useState<MaterialUiPickersDate>(new Date());
-
-  if (loggedInUser === undefined) return <CircularProgress />;
-  if (!loggedInUser) {
-    history.push('/login');
-    // loading for a split seconds until history.push works
-    return <CircularProgress />;
-  }
 
   const handleDateChange = (date: MaterialUiPickersDate): MaterialUiPickersDate => {
     setDate(date);
@@ -79,12 +65,11 @@ function Booking(): JSX.Element {
 
   return (
     <>
-      <NavBar loggedInUser={loggedInUser} />
       <Grid container spacing={6} direction="row" alignContent="center" alignItems="center" className={classes.root}>
         <Grid item xs={12} sm={6}>
-          <Card>
+          <Paper elevation={3}>
             <BookingItem title="your next booking:" upcoming />
-          </Card>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -103,10 +88,10 @@ function Booking(): JSX.Element {
           </MuiPickersUtilsProvider>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Card>
+          <Paper elevation={3}>
             <BookingItem title="current bookings:" />
             <BookingItem title="past bookings:" />
-          </Card>
+          </Paper>
         </Grid>
       </Grid>
     </>
