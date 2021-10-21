e7e7ee11d96670d78c105aa2a38048abdafb4c4c
hayderjebur <49797771+hayderjebur@users.noreply.github.com>
2021-10-21 14:11:05 -0400

    Change notRouter to notificationRouter

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const logger = require("morgan");
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
-const notRouter = require("./routes/notification");
+const notificationRouter = require("./routes/notification");
 
 const { json, urlencoded } = express;
 
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
-app.use("/notification", notRouter);
+app.use("/notification", notificationRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
