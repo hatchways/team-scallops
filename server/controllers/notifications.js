b4508c3b20356388bec0929e57318d3d5edfc592
hayderjebur <49797771+hayderjebur@users.noreply.github.com>
2021-10-21 12:28:23 -0400

    Make routes for notifications

--- server/controllers/notifications.js
+++ server/controllers/notifications.js
@@ -1,65 +1,82 @@
-const express = require("express");
-const router = express.Router();
 const Notification = require("../models/Notification");
 const asyncHandler = require("express-async-handler");
-const UserModel = require("../models/User");
 
-exports.getNotifi = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-
-  const user = await Notification.findOne({ receiver: id })
-    .populate("notifications.user")
-    .populate("notifications.post");
-
-  return res.json(user.notifications);
+// @desc    Get all Notifications
+// @route   GET /notifications
+// @access  Private
+exports.getNotifications = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const user = await Notification.findOne({ receiver: id });
+  return res.status(200).json(user.notifications);
 });
 
-const setIsReadtoTrue = async (id) => {
+// @desc    Get Unread Notifications
+// @route   GET /unreadnotifications
+// @access  Private
+exports.getUnReadNotifications = asyncHandler(async (req, res) => {
+  const { id } = req.user;
   const user = await Notification.findOne({ receiver: id });
+  const unRead = user.notifications.filter(
+    (notification) => notification.isRead === false
+  );
+  return res.status(200).json(unRead);
+});
 
-  user.notifications.isRead = true;
-  await user.save();
-};
-exports.postNotifi = asyncHandler(async (req, res) => {
-  const { revId, sendId } = req.params;
-  const { title, message } = req.body;
+// @desc    Create a new notification
+// @route   POST /notifications
+// @access  Private
+exports.createNotification = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const { senderId } = req.params;
+  const { title, message, type } = req.body;
+  const userToNotify = await Notification.findOne({ receiver: id });
 
-  const notificationTest = {
-    type: "serviceRequest",
-    receiver: revId,
-    sender: sendId,
+  if (!userToNotify) {
+    await new Notification({
+      receiver: id,
+      notifications: [
+        {
+          type,
+          sender: senderId,
+          title,
+          message,
+        },
+      ],
+    }).save();
+    return res.status(201).json(userToNotify);
+  }
+  const newNotification = {
+    type,
+    sender: senderId,
     title,
     message,
   };
-  const user = await Notification.findOne({ receiver: revId });
-  if (!user) {
-    await new Notification({
-      receiver: revId,
-      notifications: [],
-    }).save();
-  }
-
-  await user.notifications.unshift(notificationTest);
-  await user.save();
-
-  await setIsReadtoTrue(revId);
-
-  return res.json(user);
+  await userToNotify.notifications.unshift(newNotification);
+  await userToNotify.save();
+  return res.status(201).json(userToNotify);
 });
 
-// router.post("/", authMiddleware, async (req, res) => {
-//   try {
-//     const { userId } = req;
+// @desc    Update notification to read
+// @route   Patch /notification/:receiverId/:notificationId
+// @access  Private
+exports.updateNotificationToRead = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  const { notificationId } = req.params;
 
-//     const user = await UserModel.findById(userId);
+  const user = await Notification.findOne({ receiver: id });
 
-//     if (user.unreadNotification) {
-//       user.unreadNotification = false;
-//       await user.save();
-//     }
-//     return res.status(200).send("Updated");
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).send("Server Error");
-//   }
-// });
+  if (user.notifications.length > 0) {
+    const foundNotification = user.notifications.find(
+      (notification) => notification._id.toString() === notificationId
+    );
+
+    if (foundNotification) {
+      foundNotification.isRead = true;
+      const updateNotification = await user.save();
+      res.status(200).json(updateNotification);
+    } else {
+      res.status(400);
+      throw new Error("No notification found");
+    }
+  }
+});
--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getNotifi, postNotifi } = require("../controllers/notifications");
+const {
+  getNotifications,
+  createNotification,
+  getUnReadNotifications,
+  updateNotificationToRead,
+} = require("../controllers/notifications");
 const protect = require("../middleware/auth");
 
-router.route("/:id").get(getNotifi);
-router.route("/:revId/:sendId").post(postNotifi);
+router.route("/").get(protect, getNotifications);
+router.route("/unread/:id").get(protect, getUnReadNotifications);
+router.route("/:senderId").post(protect, createNotification);
+router.route("/:notificationId").patch(protect, updateNotificationToRead);
 
 module.exports = router;
