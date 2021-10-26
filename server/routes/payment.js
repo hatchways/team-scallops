28dfa3d5a11533b784007b3e875598113a0ca944
Destin Martin <destintech@gmail.com>
2021-10-25 21:39:37 -0600

    removed test files

--- server/routes/payment.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const protect = require("../middleware/auth");
-const { create } = require("../controllers/payment");
-router.route("/").post(protect, payment);
-
-module.exports = router;
