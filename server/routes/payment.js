0cb7adc818d588fa8d3487e09a8631a9fc799cbb
Destin Martin <destintech@gmail.com>
2021-11-01 17:34:54 -0600

    removed unnecessary code

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { validateRegister, validateLogin } = require("../validate");
 const protect = require("../middleware/auth");
 const { secret } = require("../controllers/payments");
 
