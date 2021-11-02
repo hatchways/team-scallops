0f513ca8661897231ab09eafb21549c7b9a41f02
hayderjebur <49797771+hayderjebur@users.noreply.github.com>
2021-11-02 11:58:57 -0400

    Add sendMsg function

--- server/utils/socketNotifications.js
+++ server/utils/socketNotifications.js
@@ -1,5 +1,6 @@
 const Conversation = require("../models/Conversation");
 const Message = require("../models/Message");
+const Notification = require("../models/Notification");
 
 const sendMsg = async (userId, msgSendToUserId, msg) => {
   try {
@@ -15,6 +16,17 @@ const sendMsg = async (userId, msgSendToUserId, msg) => {
       conversation: conversation._id,
     };
     const message = await Message.create(newMsg);
-    await message.save();
-  } catch (error) {}
+    const newMsg = await message.save();
+    return { newMsg };
+  } catch (error) {
+    return { error };
+  }
+};
+
+const sendNotification = async (notificationId, userId) => {
+  try {
+    const notification = await Notification.findOne(notificationId);
+  } catch (error) {
+    return { error };
+  }
 };
