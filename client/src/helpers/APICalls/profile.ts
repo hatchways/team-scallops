bd21a766e7fcb782018e480cd580012e7e8b056b
MarianaES <mariana.enriquez.s@gmail.com>
2021-11-03 00:45:26 -0500

    Add API calls for getUserProfile and updateProfile

--- client/src/helpers/APICalls/profile.ts
+++ client/src/helpers/APICalls/profile.ts
@@ -1,5 +1,6 @@
 import { FetchOptions } from '../../interface/FetchOptions';
 import { Profile } from '../../interface/Profile';
+import { AvailabilityInDays } from '../../interface/Profile';
 
 export async function getSitterProfile(sitterId?: string): Promise<Profile> {
   const fetchOptions: FetchOptions = {
@@ -10,4 +11,28 @@ export async function getSitterProfile(sitterId?: string): Promise<Profile> {
   return await fetch(`/profile/${sitterId}`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({ error: { message: 'Unable to connect to server. Please try again' } }));
+}
+
+export async function getUserProfile(): Promise<Profile> {
+  const fetchOptions: FetchOptions = {
+    method: 'GET',
+    credentials: 'include',
+  };
+
+  return await fetch(`/profile/`, fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({ error: { message: 'Unable to connect to server.' } }));
+}
+
+export async function updateProfile(availability: AvailabilityInDays): Promise<Profile> {
+  const fetchOptions: FetchOptions = {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ availability }),
+    credentials: 'include',
+  };
+
+  return await fetch('/profile/', fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({ error: { message: 'Unable to connect to server.' } }));
 }
