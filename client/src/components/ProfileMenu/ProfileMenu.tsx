7a247219627df850233a1532e90154b7a2ef0bde
MarianaES <mariana.enriquez.s@gmail.com>
2021-11-03 00:48:07 -0500

    Add condition to display availability only to a sitter user

--- client/src/components/ProfileMenu/ProfileMenu.tsx
+++ client/src/components/ProfileMenu/ProfileMenu.tsx
@@ -1,9 +1,12 @@
 import { Grid, Box } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../context/useAuthContext';
 import useStyles from './useStyles';
 
 export default function ProfileMenu(): JSX.Element {
   const classes = useStyles();
+
+  const { loggedInUser } = useAuth();
 
   return (
     <Box>
@@ -14,9 +17,11 @@ export default function ProfileMenu(): JSX.Element {
         <NavLink activeClassName={classes.active} className={classes.link} to="/profile/photo">
           Profile photo
         </NavLink>
-        <NavLink activeClassName={classes.active} className={classes.link} to="/profile/availability">
-          Availability
-        </NavLink>
+        {loggedInUser?.isSitter && (
+          <NavLink activeClassName={classes.active} className={classes.link} to="/profile/availability">
+            Availability
+          </NavLink>
+        )}
         <NavLink activeClassName={classes.active} className={classes.link} to="/profile/payment">
           Payment
         </NavLink>
