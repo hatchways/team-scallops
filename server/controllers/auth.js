06eea43e1efbfe0f7910f6cfb48c1d4d4be1c458
MarianaES <mariana.enriquez.s@gmail.com>
2021-11-05 02:47:31 -0500

    Integrate SendGridAPI, forgot and reset controllers and routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,10 @@ const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/generateToken");
 const { stripeSetup } = require("./payments");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { sendEmail } = require("../utils/sendEmail");
 
 // @route POST /auth/register
 // @desc Register user
@@ -119,4 +123,74 @@ exports.logoutUser = asyncHandler(async (req, res, next) => {
   res.clearCookie("token");
 
   res.send("You have successfully logged out");
+});
+
+// @route POST /auth/forgot
+// @desc Send reset password link to selected email
+// @access Public
+exports.sendResetLink = asyncHandler(async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!email) {
+      res.status(400).send({ error: "Email is required" });
+    }
+
+    if (!validator.isEmail(email)) {
+      res.status(400).send({ error: "Invalid email" });
+    }
+    if (!user) {
+      res.status(404).send({ error: "User not found" });
+    }
+
+    const token = generateToken(user._id);
+    const link = `${req.protocol}://localhost:3001/reset/${token}`;
+
+    await sendEmail(
+      email,
+      "mariana.teia.cloud@gmail.com",
+      "Reset your password",
+      `<div>Click the link below to reset your password</div><br/>
+      <div>${link}</div>
+      `
+    );
+
+    res.status(200).send({
+      message: "Password reset link has been successfullly sent to your inbox",
+    });
+  } catch (e) {
+    next(new Error(e));
+  }
+});
+
+// @route POST /auth/reset/:token
+// @desc Reset password
+// @access Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  try {
+    let { password: plainPassword } = req.body;
+
+    const { token } = req.params;
+    const decoded = jwt.decode(token);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      res.status(404).send({ error: "User not found" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(plainPassword, salt);
+
+    const updatedUser = await User.updateOne(
+      { _id: decoded.id },
+      { $set: { password: hash } },
+      { new: true }
+    );
+
+    res.status(200).send({ token, updatedUser });
+  } catch (e) {
+    next(new Error(e));
+  }
 });
