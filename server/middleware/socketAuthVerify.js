5275139cf0c2917a68820531e22d3db06b031244
Destin Martin <destintech@gmail.com>
2021-11-20 01:49:45 -0600

    added regex to split cookies delivered by socketIO

--- server/middleware/socketAuthVerify.js
+++ server/middleware/socketAuthVerify.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const socketAuthVerify = (socket, next) => {
-  const token = socket.handshake.headers.cookie.split("=")[1];
-
+  const cookieString = socket.handshake.headers.cookie;
+  const cookies = cookieString.split(/[=;]/); // regex
+  const tokenIndex = cookies.indexOf("token") + 1;
+  const token = cookies[tokenIndex];
   if (!token) {
     next(new Error("No token, authorization denied"));
   }
